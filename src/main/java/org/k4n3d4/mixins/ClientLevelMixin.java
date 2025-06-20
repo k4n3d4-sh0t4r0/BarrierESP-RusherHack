331174262f26cc1b92d579a68eae2c5ab64e1360
K4n3d4 <k4n3d4_sh0t4r0@proton.me>
2025-06-21 01:37:03 +0200

    Update ClientLevelMixin.java

--- src/main/java/org/k4n3d4/mixins/ClientLevelMixin.java
+++ src/main/java/org/k4n3d4/mixins/ClientLevelMixin.java
@@ -7,14 +7,13 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.Items;
 
+import org.k4n3d4.BarrierESPModule;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import static org.k4n3d4.BarrierESPModule.IsEnable;
 
 @Mixin(ClientLevel.class)
 public class ClientLevelMixin {
@@ -29,8 +28,10 @@ public class ClientLevelMixin {
             return;
         }
 
-        if (IsEnable) {
-            cir.setReturnValue(Blocks.BARRIER);
+        if (!BarrierESPModule.IsEnable) {
+            return;
         }
+
+        cir.setReturnValue(Blocks.BARRIER);
     }
 }
